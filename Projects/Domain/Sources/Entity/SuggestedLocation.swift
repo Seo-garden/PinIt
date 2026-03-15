032bcce8f1e1e3cb154cff798644a16d413651fe
seo-garden <indian4589@naver.com>
2026-03-15 14:27:21 +0900

    feat: SuggestedLocation 근시 비교

--- Projects/Domain/Sources/Entity/SuggestedLocation.swift
+++ Projects/Domain/Sources/Entity/SuggestedLocation.swift
@@ -8,13 +8,17 @@ import Foundation
 public struct SuggestedLocation: Equatable {
     public let coordinate: Coordinate
     public let title: String
-
+    
     public init(coordinate: Coordinate, title: String) {
         self.coordinate = coordinate
         self.title = title
     }
-
+    
     public static func == (lhs: SuggestedLocation, rhs: SuggestedLocation) -> Bool {
-        lhs.title == rhs.title && lhs.coordinate.isApproximatelyEqual(to: rhs.coordinate)
+        lhs.title == rhs.title && lhs.coordinate == rhs.coordinate
+    }
+    
+    public func isApproximatelySame(as other: SuggestedLocation, epsilon: Double = 0.00001) -> Bool {
+        title == other.title && coordinate.isApproximatelyEqual(to: other.coordinate, epsilon: epsilon)
     }
 }
