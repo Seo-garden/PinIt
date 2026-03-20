2130630028361c29b3de785bf7baade2986e88b6
mandooplz <kmw4561@gmail.com>
2026-03-20 15:12:57 +0900

    feat: add login presentation skeleton

---
+++ Projects/Presentation/Sources/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Presentation
+//
+//  Created by 김민우 on 3/20/26.
+//
+
+import UIKit
+
+
+// MARK: viewcontroller
+@MainActor
+public final class LoginViewController: UIViewController {
+    private let viewModel = LoginViewModel()
+}
---
+++ Projects/Presentation/Sources/Login/LoginViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewModel.swift
+//  Presentation
+//
+//  Created by 김민우 on 3/20/26.
+//
+
+import Foundation
+import Expose
+import OSLog
+
+
+// MARK: object
+@MainActor @Exposable
+public final class LoginViewModel {
+    private let logger = Logger()
+}
